a78ac9553de8ab57159983d232bb53837967b191
Kanegend39 <79755636+Kanegend39@users.noreply.github.com>
2024-03-10 01:40:39 +0300

    Update interferometr.py

--- interferometr.py
+++ interferometr.py
@@ -7,7 +7,7 @@ lambda0 = 1.31
 left = -300 * lambda0
 right = 300 * lambda0
 step_lambda = 1 * (10 ** (-3))
-step = 0.001
+step = 0.00001
 delta_lambda = 20. * (10 ** (-3))
 lambda_ = np.arange(lambda0 - delta_lambda, lambda0 + delta_lambda + step_lambda, step_lambda)
 delta_l = np.arange(left, right + step, step)
@@ -15,7 +15,7 @@ k = (2 * math.pi) / lambda_
 print("Диапазон λ:", lambda_)
 I1 = 0
 for i in range(len(lambda_)):
-    E = (E0 ** 2) * (math.exp(-(((lambda_[i] - lambda0) ** 2) / (2 * ((2 * delta_lambda) ** 2)))))
+    E = (E0 ** 2) * (math.exp(-(((lambda_[i] - lambda0) ** 2) / (2 * (delta_lambda ** 2)))))
     print(E)
     I1 += 2 * E * (np.cos(2 * k[i] * delta_l))
 print("Интенсивность:", I1)
