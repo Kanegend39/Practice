b8cae88907ea61af1657aef2dde9453e675827de
Kanegend39 <79755636+Kanegend39@users.noreply.github.com>
2024-03-11 23:06:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import math
 
 E0 = 1
 lambda0 = 1.31
-left = -300 * lambda0
-right = 300 * lambda0
-step_lambda = 1 * (10 ** (-3))
-step = 0.001
-delta_lambda = 20. * (10 ** (-3))
-lambda_ = np.arange(lambda0 - delta_lambda, lambda0 + delta_lambda + step_lambda, step_lambda)
+left = -300 * lambda0 #левая граница графика
+right = 300 * lambda0 #правая граница графика
+step_lambda = 1e-3 #шаг по длине волны
+step = 1e-3 #шаг по ∆l
+delta_lambda = 20e-3 
+range_lambda = np.arange(lambda0 - 5 * delta_lambda, lambda0 + 5 * delta_lambda, step_lambda) #диапазон волн
 delta_l = np.arange(left, right + step, step)
-k = (2 * math.pi) / lambda_
-print("Диапазон λ:", lambda_)
-I1 = 0
-for i in range(len(lambda_)):
-    E = (E0 ** 2) * (math.exp(-(((lambda_[i] - lambda0) ** 2) / (2 * (delta_lambda ** 2)))))
-    print(E)
-    I1 += 2 * E * (np.cos(2 * k[i] * delta_l))
-print("Интенсивность:", I1)
-plt.plot(delta_l, I1)
+print(delta_l)
+k = (2 * math.pi) / range_lambda
+print("Диапазон λ:", range_lambda)
+I = 0
+for i in range(len(range_lambda)):
+    E = (E0 ** 2) * (math.exp(-(((range_lambda[i] - lambda0) ** 2) / (2 * (delta_lambda ** 2))))) #распределение Гаусса
+    I += 2 * E * (np.cos(2 * k[i] * delta_l))
+print("Интенсивность:", I)
+plt.plot(delta_l, I)
 plt.show()
